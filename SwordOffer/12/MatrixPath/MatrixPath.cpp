b5c75ca4d870e73d8b6b6739b1aff6e8fd3f1950
ieafei2000 <ieafei2000@163.com>
2021-11-18 11:31:57 +0800

    SwordOffer012

---
+++ SwordOffer/12/MatrixPath/MatrixPath.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+class Solution {
+public:
+    bool exist(vector<vector<char>>& board, string word)
+    {
+        rows = board.size();
+        cols = board.at(0).size();
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                if (dfs(board, word, i, j, 0))
+                    return true;
+            }
+        }
+        return false;
+    }
+private:
+    int rows, cols;
+    bool dfs(vector<vector<char>>& board, string word, int i, int j, int k)
+    {
+        if (i < 0 || j < 0 || i >= rows || j >= cols || board[i][j] != word[k])
+            return false;
+        if (k == word.size() - 1)
+            return true;
+        board[i][j] = '\0';
+        bool res = dfs(board, word, i + 1, j, k + 1) || dfs(board, word, i - 1, j, k + 1) ||
+            dfs(board, word, i, j + 1, k + 1) || dfs(board, word, i, j - 1, k + 1);
+        board[i][j] = word[k];
+        return res;
+    }
+};
+
+int main()
+{
+    vector<vector<char>> board = { {'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'} };
+    string word = "ABCCED";
+    Solution sln;
+    sln.exist(board, word);
+}
+
